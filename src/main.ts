1322ab546bdb229d1059cf67fecb849903de7b8c
Dedy Prasetiady <dedy.berastagi@gmail.com>
2021-06-26 18:33:25 +0700

    Add config module

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get('ConfigService');
+  await app.listen(configService.get('port'));
 }
 bootstrap();
