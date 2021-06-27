a38da84a5a3da7b92df8cdbce7bb98bb96ecb3c7
Dedy Prasetiady <dedy.berastagi@gmail.com>
2021-06-27 08:15:17 +0700

    Add test for character service

--- src/characters/characters.service.spec.ts
+++ src/characters/characters.service.spec.ts
@@ -1,11 +1,54 @@
 import { ApiClientService } from '../api-client/api-client.service';
+import {
+  Character,
+  CharacterDataWrapper,
+  CharacterResponse,
+} from './characters.interface';
 import { CharactersService } from './characters.service';
 
 describe('CharactersService', () => {
   const apiClientService = ApiClientService.prototype;
   const service = new CharactersService(apiClientService);
 
+  const character: Character = {
+    id: 1009407,
+    name: 'Loki',
+    description: '',
+  };
+
+  const characterData: CharacterDataWrapper = {
+    data: {
+      results: [character],
+    },
+  };
+
+  const characterResponse: CharacterResponse = {
+    id: character.id,
+    name: character.name,
+    description: character.description,
+  };
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  beforeEach(async () => {
+    jest
+      .spyOn(apiClientService, 'get')
+      .mockReturnValue(Promise.resolve(characterData));
+  });
+
+  describe('getCharacter', () => {
+    it('should convert CharacterDataWrapper into CharacterResponse', async () => {
+      const result = await service.getCharacter(1);
+      expect(result).toEqual(characterResponse);
+    });
+  });
+
+  describe('getCharacters', () => {
+    it('should convert CharacterDataWrapper into number[]', async () => {
+      const result = await service.getCharacters();
+      expect(result).toEqual([character.id]);
+    });
   });
 });
